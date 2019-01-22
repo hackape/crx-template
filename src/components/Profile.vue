abe64c3a8c64a90bfc5daac22c45b28310eadb27
hackape <muse.vincent@gmail.com>
2019-01-23 02:57:03 +0800

    create Profile and Record comps

---
+++ src/components/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid>
+    <Record v-for="record in records" :key="record.id" :record="record" :remove="removeRecord.bind(this, record.id)"/>
+    <v-btn v-on:click="addRecord" >add one</v-btn>
+  </v-container>
+</template>
+
+<script>
+import Record from './Record';
+export default {
+  components: {
+    Record
+  },
+  data: () => ({
+    records: [
+      {
+        id: Date.now(),
+        enabled: true,
+        moduleId: 'some string',
+        filePath: 'some other string'
+      }
+    ]
+  }),
+  methods: {
+    addRecord() {
+      const newRecord = {
+        id: Date.now(),
+        enabled: true,
+        moduleId: '',
+        filePath: ''
+      }
+      this.records.push(newRecord);
+    },
+    removeRecord(id) {
+      this.records = this.records.filter(record => record.id !== id);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Record.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-form>
+    <v-layout row align-center>
+      <v-flex grow>
+        <v-layout row>
+          <v-flex sm5 xs5>
+            <v-layout row>
+              <v-flex shrink>
+                <v-checkbox v-model="record.enabled" />
+              </v-flex>
+              <v-flex grow>
+                <v-text-field label="Module Id" single-line v-model="record.moduleId" :disabled="!record.enabled" ></v-text-field>
+              </v-flex>
+              <v-flex shrink>&nbsp;&nbsp;&nbsp;&nbsp;</v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex sm7 xs7>
+            <v-text-field label="File Path" single-line v-model="record.filePath" :disabled="!record.enabled"></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex shrink>
+        <v-btn flat icon v-on:click="remove">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: ["record", "remove"]
+};
+</script>
+
+<style>
+</style>
