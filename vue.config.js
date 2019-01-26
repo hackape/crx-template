b8ed4cf417226cb7e1b62703f07278044e920e66
hackape <muse.vincent@gmail.com>
2019-01-26 17:23:04 +0800

    adapt vue.config.js to chrome extension building process

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+const path = require("path");
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  filenameHashing: false,
+  configureWebpack: {
+    entry: {
+      app: path.resolve(__dirname, "src/popup/main.ts")
+    }
+  }
 };
