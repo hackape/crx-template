68c3cab4b6ddd6e788f9ca456ba388334c896645
hackape <muse.vincent@gmail.com>
2019-01-28 15:25:22 +0800

    mod config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
 const path = require("path");
+
 module.exports = {
   lintOnSave: false,
   filenameHashing: false,
   configureWebpack: {
     entry: {
-      app: path.resolve(__dirname, "src/popup/main.ts")
+      app: path.resolve(__dirname, "src/popup/main.ts"),
+      background: path.resolve(__dirname, "src/background.ts"),
+      content: path.resolve(__dirname, "src/content.ts")
     }
+  },
+  chainWebpack: config => {
+    return config.plugin("html").tap(args => {
+      options = args[0];
+      options.excludeChunks = ["background", "content"];
+      options.minify = false;
+      return args;
+    });
   }
 };
